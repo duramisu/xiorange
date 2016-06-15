e97370a0fe11686cc226b1f691a74fea64cb64a2
duramisu <dyq_3344@163.com>
2016-06-15 17:23:47 +0800

    修改名称

---
+++ springmvc/src/com/orange/action/IndexController.java
@@ -0,0 +1,23 @@
+package com.orange.action;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class IndexController{
+	private static Logger logger = Logger.getLogger(IndexController.class);
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String index(HttpServletRequest request){
+		logger.warn("你好");
+		logger.info("测试");
+		return "demo/index";   
+	}
+
+}
